e6b76b94af9c340e323d0dc866a9f310161a9539
sholmx <acculumatedapples@gmail.com>
2025-03-07 21:49:10 -0600

    Update RUNTHIS.bat

--- sholmx-main/RUNTHIS.bat
+++ sholmx-main/RUNTHIS.bat
@@ -4,7 +4,7 @@ cd /d "%~dp0"
 
 echo Installing required Python packages...
 python -m pip install --upgrade pip
-python -m pip install requests customtkinter pycryptodome opencv-python pyautogui pypiwin32
+python -m pip install requests customtkinter pycryptodome opencv-python pyautogui pypiwin32 pyinstaller
 
 echo Installation complete. Starting GUI...
 python gui.py
